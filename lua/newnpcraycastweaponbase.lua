88a7ffac03df24db67baf6e5670417cc25a8757f
Sebastian Gabl <sebastian.gabl@gmx.at>
2016-10-26 16:02:19 +0200

    add utility function to get gadgets

--- lua/newnpcraycastweaponbase.lua
+++ lua/newnpcraycastweaponbase.lua
@@ -7,3 +7,28 @@ function NewNPCRaycastWeaponBase:_fire_raycast(...)
     return _fire_raycast_original(self, ...)
   end
 end
+
+function NewNPCRaycastWeaponBase:_find_gadget_type_index(gadget_type)
+  -- helper function to get the first gadget of a type
+  local gadgets = managers.weapon_factory:get_parts_from_weapon_by_type_or_perk("gadget", self._factory_id, self._blueprint) or {}
+  local xd, yd
+  table.sort(gadgets, function(x, y)
+    xd = self._parts[x]
+    yd = self._parts[y]
+    if not xd then
+      return false
+    end
+    if not yd then
+      return true
+    end
+    return xd.unit:base().GADGET_TYPE > yd.unit:base().GADGET_TYPE
+  end)
+  local index
+  local gadget
+  for i, id in ipairs(gadgets) do
+    gadget = self._parts[id]
+    if gadget and gadget.unit and gadget.unit:base().GADGET_TYPE == gadget_type then
+      return i, gadgets
+    end
+  end
+end

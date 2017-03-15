4fca50c46e10a59edc752bd891c020a375f4f1ea
Sebastian Gabl <sebastian.gabl@gmx.at>
2017-03-15 19:36:49 +0100

    Fix bug in previous commit

--- lua/huskplayermovement.lua
+++ lua/huskplayermovement.lua
@@ -4,11 +4,11 @@ function HuskPlayerMovement:clbk_inventory_event(...)
   -- get reload animation from new anim field instead of usage
   local weapon = self._unit:inventory():equipped_unit()
   if weapon then
-    if self._weapon_hold then
-      self._machine:set_global(self._weapon_hold, 0)
+    if self._weapon_anim_global then
+      self._machine:set_global(self._weapon_anim_global, 0)
     end
     local weap_tweak = weapon:base():weapon_tweak_data()
     self._machine:set_global(weap_tweak.anim, 1)
-    self._weapon_hold = weap_tweak.anim
+    self._weapon_anim_global = weap_tweak.anim
   end
 end

a8ad680b761bb62af4116808cde47bdc8617f951
Sebastian Gabl <sebastian.gabl@gmx.at>
2017-04-19 22:00:10 +0200

    Remove newnpcraycastweaponbase.lua
    Now implemented in the NPC Shotgun Pellets mod itself

--- lua/newnpcraycastweaponbase.lua
+++
@@ -1,9 +0,0 @@
-local _fire_raycast_original = NewNPCRaycastWeaponBase._fire_raycast
-function NewNPCRaycastWeaponBase:_fire_raycast(...)
-  -- use NPCShotgunBase to fire raycast if it is a shotgun to be compatible with my NPC Shotgun Pellets mod
-  if self:weapon_tweak_data().is_shotgun and NPCShotgunBase._fire_raycast then
-    return NPCShotgunBase._fire_raycast(self, ...)
-  else
-    return _fire_raycast_original(self, ...)
-  end
-end

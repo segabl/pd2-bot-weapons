783f39e311ffbbfe7bee958a51ef6a2ee1e4fca0
Sebastian Gabl <sebastian.gabl@gmx.at>
2017-09-30 01:13:28 +0200

    Use mean of dmg_mul for better damage estimate

--- lua/copactionshoot.lua
+++ lua/copactionshoot.lua
@@ -1,4 +1,16 @@
-dofile(ModPath .. "botweapons.lua")
+local function mean(tbl, member)
+  local sum = 0
+  if member then
+    for _, v in ipairs(tbl) do
+      sum = sum + v[member]
+    end
+  else
+    for _, v in ipairs(tbl) do
+      sum = sum + v
+    end
+  end
+  return sum / #tbl
+end
 
 local init_original = CopActionShoot.init
 function CopActionShoot:init(action_desc, common_data, ...)
@@ -10,18 +22,18 @@ function CopActionShoot:init(action_desc, common_data, ...)
       if not CopActionShoot.TEAM_AI_TARGET_DAMAGE then
         -- calculate m4 dps as target dps for other weapons
         local damage = tweak_data.weapon.m4_crew.DAMAGE
-        local dmg_mul = common_data.char_tweak.weapon.is_rifle.FALLOFF[1].dmg_mul
+        local dmg_mul = mean(common_data.char_tweak.weapon.is_rifle.FALLOFF, "dmg_mul")
         local mag =  tweak_data.weapon.m4_crew.CLIP_AMMO_MAX
-        local burst_size = (common_data.char_tweak.weapon.is_rifle.autofire_rounds[1] + common_data.char_tweak.weapon.is_rifle.autofire_rounds[2]) * 0.5
+        local burst_size = mean(common_data.char_tweak.weapon.is_rifle.autofire_rounds)
         local shot_delay =  tweak_data.weapon.m4_crew.auto.fire_rate
         local burst_delay = tweak_data.weapon.m4_crew.burst_delay
         local reload = common_data.char_tweak.weapon.is_rifle.RELOAD_SPEED
         CopActionShoot.TEAM_AI_TARGET_DAMAGE = (damage * dmg_mul * mag) / ((mag / burst_size) * (burst_size - 1) * shot_delay + (mag / burst_size - 1) * burst_delay + reload)
       end
       -- calculate weapon damage based on m4 dps
-      local dmg_mul = w_u_tweak.FALLOFF[1].dmg_mul
+      local dmg_mul = mean(w_u_tweak.FALLOFF, "dmg_mul")
       local mag = w_tweak.CLIP_AMMO_MAX
-      local burst_size = w_tweak.fire_mode == "auto" and w_u_tweak.autofire_rounds and (w_u_tweak.autofire_rounds[1] + w_u_tweak.autofire_rounds[2]) * 0.5 or 1
+      local burst_size = w_tweak.fire_mode == "auto" and w_u_tweak.autofire_rounds and mean(w_u_tweak.autofire_rounds) or 1
       local shot_delay = w_tweak.auto.fire_rate
       local burst_delay = w_tweak.burst_delay
       local reload = w_u_tweak.RELOAD_SPEED
@@ -32,7 +44,7 @@ function CopActionShoot:init(action_desc, common_data, ...)
       for _, v in ipairs(w_tweak.falloff) do
         v.recoil = recoil
       end
-      BotWeapons:log(self._weapon_base._name_id .. ": damage = " .. self._weapon_base._damage .. ", recoil = " .. recoil[1], BotWeapons.debug)
+      --con:print(self._weapon_base._name_id .. ": damage = " .. self._weapon_base._damage .. ", recoil = " .. recoil[1])
     end
     self._falloff = w_tweak.falloff
     self._automatic_weap = w_tweak.fire_mode == "auto" and w_u_tweak.autofire_rounds and true

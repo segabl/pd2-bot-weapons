9e196b504667e014722920bf00589b879c9faad4
Sebastian Gabl <sebastian.gabl@gmx.at>
2017-12-02 11:40:16 +0100

    Return result of original spawn_unit

--- lua/networkpeer.lua
+++ lua/networkpeer.lua
@@ -1,12 +1,13 @@
 dofile(ModPath .. "botweapons.lua")
 
 local spawn_unit_original = NetworkPeer.spawn_unit
-function NetworkPeer:spawn_unit(spawn_point_id, is_drop_in, spawn_as)
-  spawn_unit_original(self, spawn_point_id, is_drop_in, spawn_as)
+function NetworkPeer:spawn_unit(spawn_point_id, is_drop_in, ...)
+  local unit = spawn_unit_original(self, spawn_point_id, is_drop_in, ...)
   -- handle sync with dropped in peers
   if is_drop_in and BotWeapons._data.sync_settings and Network:is_server() then
     for _, data in pairs(managers.groupai:state():all_AI_criminals()) do
       BotWeapons:sync_to_peer(self, data.unit)
     end
   end
+  return unit
 end

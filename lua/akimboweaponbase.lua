d3d5cbc0d33d10dadbaaf8a9edcab1e3078fd3e1
Sebastian Gabl <sebastian.gabl@gmx.at>
2018-03-22 02:28:36 +0100

    Remove weapon fire animation
    Added to NPC weapon customizer

--- lua/akimboweaponbase.lua
+++ lua/akimboweaponbase.lua
@@ -7,16 +7,10 @@ function NPCAkimboWeaponBase:fire(...)
   if self._fire_second_gun_next then
     if alive(self._second_gun) then
       result = self._second_gun:base().super.fire(self._second_gun:base(), ...)
-      if result then
-        self._second_gun:base():tweak_data_anim_play("fire")
-      end
     end
     self._fire_second_gun_next = false
   else
     result = NPCAkimboWeaponBase.super.fire(self, ...)
-    if result then
-      NPCAkimboWeaponBase.super.tweak_data_anim_play(self, "fire")
-    end
     self._fire_second_gun_next = true
   end
   return result

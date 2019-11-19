6ff981f5ab7c08a64037bdcef16015d8feb471bd
Sebastian Gabl <sebastian.gabl@gmx.at>
2019-11-19 11:21:53 +0100

    Add compatibility with CASS

--- lua/copactionshoot.lua
+++ lua/copactionshoot.lua
@@ -62,30 +62,34 @@ function TeamAIActionShoot:init(...)
   end
 end
 
--- interpolate damage for team ai
-local lerp = math.lerp
-function TeamAIActionShoot:_get_shoot_falloff(target_dis, falloff)
-  local i = #falloff
-  local data = falloff[i]
-  for i_range, range_data in ipairs(falloff) do
-    if target_dis < range_data.r then
-      i = i_range
-      data = range_data
-      break
+if not CASS then
+
+  -- interpolate damage for team ai
+  local lerp = math.lerp
+  function TeamAIActionShoot:_get_shoot_falloff(target_dis, falloff)
+    local i = #falloff
+    local data = falloff[i]
+    for i_range, range_data in ipairs(falloff) do
+      if target_dis < range_data.r then
+        i = i_range
+        data = range_data
+        break
+      end
+    end
+    if i == 1 or target_dis > data.r then
+      return data, i
+    else
+      local prev_data = falloff[i - 1]
+      local t = (target_dis - prev_data.r) / (data.r - prev_data.r)
+      local n_data = {
+        dmg_mul = lerp(prev_data.dmg_mul, data.dmg_mul, t),
+        r = target_dis,
+        acc = { lerp(prev_data.acc[1], data.acc[1], t), lerp(prev_data.acc[2], data.acc[2], t) },
+        recoil = { lerp(prev_data.recoil[1], data.recoil[1], t), lerp(prev_data.recoil[2], data.recoil[2], t) },
+        mode = data.mode
+      }
+      return n_data, i
     end
   end
-  if i == 1 or target_dis > data.r then
-    return data, i
-  else
-    local prev_data = falloff[i - 1]
-    local t = (target_dis - prev_data.r) / (data.r - prev_data.r)
-    local n_data = {
-      dmg_mul = lerp(prev_data.dmg_mul, data.dmg_mul, t),
-      r = target_dis,
-      acc = { lerp(prev_data.acc[1], data.acc[1], t), lerp(prev_data.acc[2], data.acc[2], t) },
-      recoil = { lerp(prev_data.recoil[1], data.recoil[1], t), lerp(prev_data.recoil[2], data.recoil[2], t) },
-      mode = data.mode
-    }
-    return n_data, i
-  end
+
 end

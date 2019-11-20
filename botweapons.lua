506c69587728ad4c3e0fa16dbd5d2d21f23b6041
Sebastian Gabl <sebastian.gabl@gmx.at>
2019-11-20 18:04:42 +0100

    Add sanity check for custom weapon mods

--- botweapons.lua
+++ botweapons.lua
@@ -23,6 +23,8 @@ if not BotWeapons then
     "akimbo",
     "pistol"
   }
+  
+  local unit_ids = Idstring("unit")
 
   function BotWeapons:log(message, condition)
     if condition or condition == nil then
@@ -289,7 +291,10 @@ if not BotWeapons then
       if math.random() < self._data.weapon_customized_chance then
         local part_data = table.random(parts_data)
         if part_data then
-          managers.weapon_factory:change_part_blueprint_only(weapon.factory_id, part_data[1], weapon.blueprint)
+          factory_data = tweak_data.weapon.factory.parts[part_data[1]]
+          if factory_data and (not factory_data.custom or factory_data.third_unit and DB:has(unit_ids, factory_data.third_unit:id())) then
+            managers.weapon_factory:change_part_blueprint_only(weapon.factory_id, part_data[1], weapon.blueprint)
+          end
         end
       end
     end

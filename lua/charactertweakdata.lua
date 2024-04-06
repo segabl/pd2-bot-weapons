9b0e9b902a8e0d05d56cd855ab2166ea78ea635e
Sebastian Gabl <sebastian.gabl@gmx.at>
2024-04-06 11:50:26 +0200

    Increase bot weapon falloff range

--- lua/charactertweakdata.lua
+++ lua/charactertweakdata.lua
@@ -9,52 +9,46 @@ function CharacterTweakData:_presets(tweak_data, ...)
 	local crew_presets = deep_clone(presets.weapon.gang_member)
 
 	local difficulty_index = tweak_data:difficulty_to_index(Global.game_settings and Global.game_settings.difficulty or "normal")
-	local rifle_dmg_mul = difficulty_index * 0.75
+	local rifle_dmg_mul = difficulty_index * 0.7
 
 	-- rifle preset
-	crew_presets.is_rifle.RELOAD_SPEED = 1
 	crew_presets.is_rifle.FALLOFF = {
 		{ dmg_mul = rifle_dmg_mul * 0.7, r = 0, acc = { 1, 1 }, recoil = { 1, 1 }, mode = { 0, 0, 0, 1 } },
-		{ dmg_mul = rifle_dmg_mul * 0.3, r = 3000, acc = { 1, 1 }, recoil = { 1, 1 }, mode = { 0, 0, 0, 1 } }
+		{ dmg_mul = rifle_dmg_mul * 0.3, r = 4000, acc = { 1, 1 }, recoil = { 1, 1 }, mode = { 0, 0, 0, 1 } }
 	}
 
 	-- sniper preset
-	crew_presets.is_sniper.RELOAD_SPEED = 1
 	crew_presets.is_sniper.FALLOFF = {
 		{ dmg_mul = rifle_dmg_mul * 0.5, r = 0, acc = { 1, 1 }, recoil = { 1, 1 }, mode = { 1, 0, 0, 0 } },
-		{ dmg_mul = rifle_dmg_mul * 0.5, r = 3000, acc = { 1, 1 }, recoil = { 1, 1 }, mode = { 1, 0, 0, 0 } }
+		{ dmg_mul = rifle_dmg_mul * 0.5, r = 4000, acc = { 1, 1 }, recoil = { 1, 1 }, mode = { 1, 0, 0, 0 } }
 	}
 
 	-- pistol preset
-	crew_presets.is_pistol.RELOAD_SPEED = 1
 	crew_presets.is_pistol.FALLOFF = {
 		{ dmg_mul = rifle_dmg_mul * 0.8, r = 0, acc = { 1, 1 }, recoil = { 1, 1 }, mode = { 1, 0, 0, 0 } },
-		{ dmg_mul = rifle_dmg_mul * 0.2, r = 3000, acc = { 1, 1 }, recoil = { 1, 1 }, mode = { 1, 0, 0, 0 } }
+		{ dmg_mul = rifle_dmg_mul * 0.2, r = 4000, acc = { 1, 1 }, recoil = { 1, 1 }, mode = { 1, 0, 0, 0 } }
 	}
 
 	-- LMG preset
-	crew_presets.is_lmg.RELOAD_SPEED = 1
 	crew_presets.is_lmg.FALLOFF = {
 		{ dmg_mul = rifle_dmg_mul * 0.9, r = 0, acc = { 1, 1 }, recoil = { 1, 1 }, mode = { 0, 0, 0, 1 } },
-		{ dmg_mul = rifle_dmg_mul * 0.1, r = 3000, acc = { 1, 1 }, recoil = { 1, 1 }, mode = { 0, 0, 0, 1 } }
+		{ dmg_mul = rifle_dmg_mul * 0.1, r = 4000, acc = { 1, 1 }, recoil = { 1, 1 }, mode = { 0, 0, 0, 1 } }
 	}
 
 	-- shotgun preset
 	crew_presets.is_shotgun_pump.RELOAD_SPEED = 1
 	crew_presets.is_shotgun_pump.FALLOFF = {
 		{ dmg_mul = rifle_dmg_mul * 1.0, r = 0, acc = { 1, 1 }, recoil = { 1, 1 }, mode = { 1, 0, 0, 0 } },
-		{ dmg_mul = rifle_dmg_mul * 0.0, r = 3000, acc = { 1, 1 }, recoil = { 1, 1 }, mode = { 1, 0, 0, 0 } }
+		{ dmg_mul = rifle_dmg_mul * 0.0, r = 4000, acc = { 1, 1 }, recoil = { 1, 1 }, mode = { 1, 0, 0, 0 } }
 	}
 
 	-- auto shotgun preset
-	crew_presets.is_shotgun_mag.RELOAD_SPEED = 1
 	crew_presets.is_shotgun_mag.FALLOFF = {
 		{ dmg_mul = rifle_dmg_mul * 1.0, r = 0, acc = { 1, 1 }, recoil = { 1, 1 }, mode = { 0, 0, 0, 1 } },
-		{ dmg_mul = rifle_dmg_mul * 0.0, r = 3000, acc = { 1, 1 }, recoil = { 1, 1 }, mode = { 0, 0, 0, 1 } }
+		{ dmg_mul = rifle_dmg_mul * 0.0, r = 4000, acc = { 1, 1 }, recoil = { 1, 1 }, mode = { 0, 0, 0, 1 } }
 	}
 
 	-- flamethrower preset
-	crew_presets.is_flamethrower.RELOAD_SPEED = 1
 	crew_presets.is_flamethrower = deep_clone(crew_presets.is_lmg)
 	crew_presets.is_flamethrower.range = { optimal = 900, far = 1800, close = 450 }
 	crew_presets.is_flamethrower.FALLOFF = {
